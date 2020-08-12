be60c492ef17eb1db58ff5e433fd3bd0efa72ea3
Andy Boedo <andresboedo@gmail.com>
2020-08-12 18:16:54 -0300

    added docs and extracted method for variable length quantity decoder

--- Purchases/SwiftSources/LocalReceiptParsing/Builders/ASN1ObjectIdentifierBuilder.swift
+++ Purchases/SwiftSources/LocalReceiptParsing/Builders/ASN1ObjectIdentifierBuilder.swift
@@ -6,6 +6,8 @@
 import Foundation
 
 class ASN1ObjectIdentifierBuilder {
+
+    // info on the format: https://docs.microsoft.com/en-us/windows/win32/seccertenroll/about-object-identifier
     func build(fromPayload payload: ArraySlice<UInt8>) -> ASN1ObjectIdentifier? {
         guard let firstByte = payload.first else { return nil }
 
@@ -14,21 +16,33 @@ class ASN1ObjectIdentifierBuilder {
         objectIdentifierNumbers.append(UInt(firstByte % 40))
 
         let trailingPayload = payload.dropFirst()
+        let variableLengthQuantityNumbers = decodeVariableLengthQuantity(payload: trailingPayload)
+        objectIdentifierNumbers += variableLengthQuantityNumbers
+
+        let objectIdentifierString = objectIdentifierNumbers.map { String($0) }
+                                                            .joined(separator: ".")
+        return ASN1ObjectIdentifier(rawValue: objectIdentifierString)
+    }
+}
+
+private extension ASN1ObjectIdentifierBuilder {
+
+    // https://en.wikipedia.org/wiki/Variable-length_quantity
+    func decodeVariableLengthQuantity(payload: ArraySlice<UInt8>) -> [UInt] {
+        var decodedNumbers = [UInt]()
+
         var currentBuffer: UInt = 0
         var isShortLength = false
-        for byte in trailingPayload {
+        for byte in payload {
             isShortLength = byte.bitAtIndex(0) == 0
             let byteValue = UInt(byte.valueInRange(from: 1, to: 7))
 
             currentBuffer = (currentBuffer << 7) | byteValue
             if isShortLength {
-                objectIdentifierNumbers.append(currentBuffer)
+                decodedNumbers.append(currentBuffer)
                 currentBuffer = 0
             }
         }
-
-        let objectIdentifierString = objectIdentifierNumbers.map { String($0) }
-                                                            .joined(separator: ".")
-        return ASN1ObjectIdentifier(rawValue: objectIdentifierString)
+        return decodedNumbers
     }
 }
